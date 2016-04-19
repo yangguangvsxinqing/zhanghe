df6faf206d4baabe93eac584fbdc6bf6b23a189d
zhanghe <zhanghe@huaqin.com>
2016-04-19 17:22:42 +0800

    [master][FineOSFileExplorer][zhanghe]:添加ROM2.0

---
+++ FineOSFileExplorer/app/build/generated/source/buildConfig/ROM2/debug/com/fineos/fileexplorer/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fineos.fileexplorer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fineos.fileexplorer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "ROM2";
+  public static final int VERSION_CODE = 24;
+  public static final String VERSION_NAME = "ROM2.020160419";
+  // Fields from default config.
+  public static final boolean DEFAULT_STORAGE_PATH = true;
+  public static final boolean DEFAULT_STORAGE_PATH_zaw875 = false;
+}
